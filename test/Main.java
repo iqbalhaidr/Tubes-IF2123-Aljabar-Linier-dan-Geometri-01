d277bf836f13b065ecad45e9df70d8b2ab56f18e
Ferdinand18780 <ferdinandgts5@gmail.com>
2024-10-10 00:28:26 +0700

    Add test cases for cofactor determinant function

--- test/Main.java
+++ test/Main.java
@@ -2,19 +2,24 @@ package test;
 
 
 import Library.OperasiDasarMatrix;
+import Library.Determinan;
 import Library.EliminasiGaus;
 import Library.Matrix;
+
 
 public class Main {
     public static void main(String[] args){
         OperasiDasarMatrix ODM = new OperasiDasarMatrix();
         EliminasiGaus gauss = new EliminasiGaus();
+        Determinan cofactor = new Determinan();
+        double hasilDet;
 
         Matrix matriks = new Matrix();
         System.out.println("Silahkan masukkan matriks anda:");
-        ODM.createMatrix(matriks,4,5);
-        ODM.readMatrix(matriks, 4, 5);
+        ODM.createMatrix(matriks,4,4);
+        ODM.readMatrix(matriks, 4, 4);
 
+        // mencoba eliminasi gauss
         System.out.println("Matriks Sebelum:");
         ODM.displayMatrix(matriks);
 
@@ -22,6 +27,12 @@ public class Main {
 
         System.out.println("Matriks Sesudah:");
         ODM.displayMatrix(matriks);
+
+
+        // mencoba determinan kofaktor
+        hasilDet=cofactor.kofaktor(matriks);
+        System.out.printf("Hasil Kofaktor: %f",hasilDet);
+
 
     }
 }
