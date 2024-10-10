b07ca6c8a9c5157b139bf3197f4e134015399e5b
iqbalhaidr <13523111@std.stei.itb.ac.id>
2024-10-11 01:13:59 +0700

    feat: SPLInverse

--- src/Library/SPL.java
+++ src/Library/SPL.java
@@ -4,10 +4,11 @@ public class SPL {
 
     OperasiDasarMatrix ODM = new OperasiDasarMatrix();
     Determinan DET = new Determinan();
+    MatriksBalikan MB = new MatriksBalikan();
 
-    //prekondisi: determinan A != 0
-    //parameter : matriksAug adalah matriks augmented dengan n (persamaan & peubah) 
-    //return    : array 1 dimensi [X1, X2, X3, ..., Xn]
+    /*prekondisi: det(A) != 0 (solusi unik)
+      parameter : matriksAug adalah matriks augmented dengan n (persamaan & peubah) 
+      return    : array 1 dimensi [X1, X2, X3, ..., Xn] */
     public double[] SPLCramer (Matrix matriksAug) {
         double[] answer = new double[matriksAug.get_COL_EFF()-1];
         double[] b = new double[matriksAug.get_ROW_EFF()];
@@ -31,12 +32,33 @@ public class SPL {
             }
 
             for (int k = 0; k < b.length; k++) {
-                ATemp.set_ELMT(k, x, b[k]);                 //set Aj
+                ATemp.set_ELMT(k, x, b[k]);                 //set Aj pada ATemp
             }
 
             answer[x] = DET.kofaktor(ATemp)/determinan;     //simpan solusi tiap peubah
         }
         
         return answer;
+    }
+
+    /*prekondisi: A memiliki inverse (solusi unik)
+      parameter : matriksAug adalah matriks augmented 
+      return    : matriks Nx1 [X1, X2, X3, ..., Xn] */
+    public Matrix SPLInverse (Matrix matriksAug) {
+        //Matrix answer = new Matrix();
+        Matrix A = new Matrix();
+        Matrix b = new Matrix();
+        //ODM.createMatrix(answer, matriksAug.get_ROW_EFF(), 1);
+        ODM.createMatrix(A, matriksAug.get_ROW_EFF(), matriksAug.get_COL_EFF()-1);
+        ODM.createMatrix(b, matriksAug.get_ROW_EFF(), 1);
+
+        for (int i = 0; i < matriksAug.get_ROW_EFF(); i++) {
+            for (int j = 0; j < matriksAug.get_COL_EFF()-1; j++) {
+                A.set_ELMT(i, j, matriksAug.get_ELMT(i, j)); 
+            b.set_ELMT(i, 0, matriksAug.get_ELMT(i, j+1));
+            }
+        }
+
+        return ODM.multiplyMatrix(MB.inverseWithAdj(A), b);
     }
 }
