7868d18952e95347854896e3c8cd6cdf28fa7c0d
iqbalhaidr <13523111@std.stei.itb.ac.id>
2024-10-17 12:33:58 +0700

    integrate regresi berganda to MyApp.java

--- test/testRegresiKuadratik.java
+++ test/testRegresiKuadratik.java
@@ -12,9 +12,7 @@ public class testRegresiKuadratik {
         //gaussjordan GJ = new gaussjordan();
         
         Matrix matriksAug = new Matrix();
-        ODM.createMatrix(matriksAug, 5, 4); //asumsi 10 data, 5 peubah
-        ODM.readMatrix(matriksAug, matriksAug.get_ROW_EFF(), matriksAug.get_COL_EFF());
-
+        matriksAug = ODM.readRegresi();
         //System.out.println(Arrays.toString(GJ.solveSPL(RB.solveRegresiKuadratik(matriksAug))));
         //System.out.println(Arrays.toString(RB.solveRegresiKuadratik(matriksAug)));
         //RB.RegresiLinear(matriksAug);
